716edd778f204312d0924fb1fec62d3369d13c4e
pipych <107928032+pipych@users.noreply.github.com>
2026-06-20 22:19:57 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Вставь сюда свои данные прямо текстом в кавычках:
-const supabaseUrl = "https://supabase.com/dashboard/project/sxdswqgoqgvkmdknqzir"; 
+const supabaseUrl = "https://sxdswqgogvkmndknqzir.supabase.co";
 const supabaseAnonKey = "sb_publishable_7_PN5q0F5n9wdnnSm4xiLQ_vHEftQL9";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
